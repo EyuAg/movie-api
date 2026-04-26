339d17721220c2c119aacc41ee95836e35f8e3fc
EyuAg <eyuelagegnehu@gmail.com>
2026-04-26 20:14:44 +0300

    implement initial server setup with movie data handling

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const filePath = path.join(__dirname, 'data.json');
+
+function readData(callback) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            callback([]);
+        } else {
+            callback(JSON.parse(data || '[]'));
+        }
+    });
+}
+
+function writeData(data, callback) {
+    fs.writeFile(filePath, JSON.stringify(data, null, 2), callback);
+}
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/movies' && method === 'GET') {
+        readData((movies) => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(movies));
+        });
+    }
+
+    else if (url.startsWith('/movies/') && method === 'GET') {
+        const id = parseInt(url.split('/')[2]);
+
+        readData((movies) => {
+            const movie = movies.find(m => m.id === id);
+
+            if (!movie) {
+                res.writeHead(404);
+                return res.end('Movie not found');
+            }
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(movie));
+        });
+    }
+
+    else {
+        res.writeHead(404);
+        res.end('Route not found');
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
